1f06cd538df516b85965f236d9158e755e940565
NoemiHeusler <58665516+noemiheusler@users.noreply.github.com>
2020-02-27 02:36:36 +0100

    minutes are bold if tram leaves in 5min or less

--- index.js
+++ index.js
@@ -9,15 +9,25 @@ const departureHome = () => {
       console.log(hours)
       console.log(minutes)
       const stationboard = data.stationboard.reverse();
-      const tbody = document.querySelector("tbody")
+      const tbody = document.querySelector("tbody");
 
       stationboard.forEach((station) => {
         ziel = station.to
         abfahrt = new Date(station.stop.departure)
         today = new Date()
         zeit = Math.round(Math.abs(today - abfahrt) / 60000)
-        row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td>${zeit} min</td></tr`
-        tbody.insertAdjacentHTML("afterbegin", row)
+        if (zeit < 6) {
+          row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td class="time">${zeit} min</td></tr`
+          tbody.insertAdjacentHTML("afterbegin", row)
+        } else {
+          row = `<tr><th scope="row"></th><td>Linie 12</td><td>${ziel}</td><td>${zeit} min</td></tr`
+          tbody.insertAdjacentHTML("afterbegin", row)
+        }
+      });
+
+      const times = document.querySelectorAll(".time");
+      times.forEach((time) =>{
+        time.classList.add("bold-font");
       });
     });
 };
@@ -28,4 +38,4 @@ const lager = () => {
       const tbody = document.querySelector("tbody")
       const row = '<tr><th scope="row"></th><td>Linie 12</td><td>Ziel</td><td>Abfahrt in</td></tr>'
       tbody.insertAdjacentHTML("afterbegin", row);
-}
+} 
